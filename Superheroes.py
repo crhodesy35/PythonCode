8fe351cb14ee94c4b00208019fe150c61c78db05
crhodesy35 <60574362+crhodesy35@users.noreply.github.com>
2020-02-06 23:06:13 -0500

    Add files via upload
    Code for creating lists of superheroes and determining whether they are Lego heroes.

---
+++ Superheroes.py
@@ -0,0 +1,8 @@
+class Superhero:
+    def __init__(self, name, super_power, nemesis, are_they_lego, gender):
+        self.name = name
+        self.super_power = super_power
+        self.nemesis = nemesis
+        self.are_they_lego = are_they_lego
+        self.gender = gender
+
